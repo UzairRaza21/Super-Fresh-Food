19a5962e6e9a73f682ecd971554771418ad71ae0
Uzair-Raza <uzairraza0@gmail.com>
2024-05-06 23:29:24 +0500

    update

--- Assets/js/fresh.js
+++ Assets/js/fresh.js
@@ -15,3 +15,35 @@ function scrolldown(){
    stickyNav.classList.remove("fixed");
   }
 }
+
+// Brands Slider Slick
+
+window.onload=function(){
+  $('.slider').slick({
+  autoplay:true,
+  autoplaySpeed:1500,
+  arrows:false,
+  //prevArrow:'<button type="button" class="slick-prev"></button>',
+  //nextArrow:'<button type="button" class="slick-next"></button>',
+  centerMode:true,
+  slidesToShow:1,
+  slidesToScroll:1
+  });
+};
+
+
+// Brand Sneak Menu Slider Slick
+$(document).ready(function(){
+  $('.center-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    centerMode: true,
+    arrows: true,
+    dots: false,
+    speed: 300,
+    centerPadding: '0px',
+    infinite: true,
+    autoplaySpeed: 5000,
+    autoplay: true
+  });
+});
